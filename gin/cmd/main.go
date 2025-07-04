dbdb5a2f3122215b90ceeeac8a1d726b98352ce1
kizunaiix <yaodong.sun@outlook.com>
2025-07-04 23:55:32 +0200

    add zap

--- gin/cmd/main.go
+++ gin/cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+	"go.uber.org/zap"
 	"gopkg.in/yaml.v3"
 	_ "ki9.com/gin_demo/cmd/docs" // swagger:这里要用你的实际 `docs` 路径
 	"ki9.com/gin_demo/internal/handler"
@@ -16,8 +17,16 @@ import (
 
 var cfg = conf.Conf{}
 
-func init() {
+// @title 习惯养成 API 文档
+// @version 0.3.2
+// @description 这是一个基于 Gin 和 RPC 风格的习惯养成 API。应当只有开发环境下能看到该文档。
+// @host localhost:9000
+// @BasePath /api
+func main() {
 
+	// 1. 加载环境变量
+
+	// 2. 加载配置
 	//加载conf文件的内容
 	confFile, err := os.ReadFile("../conf/ginconf-prod.yml")
 	if err != nil {
@@ -27,15 +36,17 @@ func init() {
 		panic(err)
 	}
 
-	// TODO 加上日志
-}
+	// 3. //TODO 初始化 logger
+	ZapLogger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	defer ZapLogger.Sync()
+	ZapLogger.Info("Logger initialized", zap.String("env", os.Getenv("ENV")))
 
-// @title 习惯养成 API 文档
-// @version 0.3.2
-// @description 这是一个基于 Gin 和 RPC 风格的习惯养成 API。应当只有开发环境下能看到该文档。
-// @host localhost:9000
-// @BasePath /api
-func main() {
+	// 4. 初始化数据库
+
+	// 5. 初始化 Gin
 	r := gin.Default()
 
 	//CORS策略
@@ -50,9 +61,16 @@ func main() {
 		r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	}
 
-	//注册路由
+	// 6. 注册路由
 	handler.RegisterRouters(r)
 
-	r.Run("0.0.0.0:9000")
+	// 7. 注册 Swagger
 
+	// 8. 启动服务
+	ZapLogger.Info("Gin server starting...",
+		zap.String("port", "9000"),
+		zap.String("env", os.Getenv("ENV")),
+	)
+
+	r.Run("0.0.0.0:9000")
 }
--- gin/go.mod
+++ gin/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/swaggo/files v1.0.1
 	github.com/swaggo/gin-swagger v1.6.0
 	github.com/swaggo/swag v1.16.4
+	go.uber.org/zap v1.27.0
 )
 
 require (
@@ -21,6 +22,7 @@ require (
 	github.com/mailru/easyjson v0.9.0 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/tools v0.30.0 // indirect
 )
 
