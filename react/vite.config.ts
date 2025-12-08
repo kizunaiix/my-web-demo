d05c3256da41c8784fdf43b78095dd86cd202244
kizunai9 <yaodong.sun@outlook.com>
2025-12-09 00:12:02 +0100

    新加了vite-react前端项目，在vite配置中添加了tailwindcss支持。并修改Taskfile.yml以支持前端构建和集成到Docker镜像中

---
+++ react/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  build: {
+    outDir: '../gin/public/', // 打包输出目录，默认就是 'dist'
+  }
+})
