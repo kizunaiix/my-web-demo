a4e80c0cb073371eecbac135e43ed5b16e2cb706
kizunaiix <yaodong.sun@outlook.com>
2025-12-16 22:48:39 +0100

    .

--- next-deprecated/build-deploy.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-npm run build
-
-docker build -t kizunaiix/my-web-demo-frontend:latest .
-
-# 构建容器
-# docker run -itd --name my-web-demo-frontend -p 3000:3000 kizunaiix/my-web-demo-frontend:latest
--- next-deprecated/src/app/tic-tac-toe/page.tsx
+++
@@ -1,9 +0,0 @@
-// TODO 学习井字棋
-
-export default function page() {
-    return (
-        <main>
-
-        </main>
-    );
-}
