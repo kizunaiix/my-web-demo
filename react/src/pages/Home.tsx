39264b9fafde183ada92b22a83cf34e7babb725f
kizunaiix <yaodong.sun@outlook.com>
2025-12-22 00:15:21 +0100

    搜索框组件

--- react/src/pages/Home.tsx
+++ react/src/pages/Home.tsx
@@ -1,19 +1,50 @@
-import reactLogo from "../assets/react.svg";
-import viteLogo from "/vite.svg";
-// import viteLogo from "";
-import { useState } from "react";
-
 export function Home() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <SearchInput type="text" />
+      <main>
+        <SearchInput
+          inputClassName="" // TODO classname的命名是不是要优化
+          buttonClassName="bg-blue-300"
+          buttonOnClick={() => {
+            window.alert("该跳转搜索结果");
+          }}
+        />
+      </main>
     </>
   );
 }
 
-export function SearchInput({type}) {
-  return (<>
-  </>)
+// SearchInput组件
+import clsx from "clsx";  // TODO clsx没法处理覆盖的问题
+
+interface SearchInputProps {
+  inputClassName?: string;
+  buttonClassName?: string;
+  buttonOnClick: () => void;
+}
+
+export function SearchInput({
+  inputClassName,
+  buttonClassName,
+  buttonOnClick,
+}: SearchInputProps) {
+  return (
+    <>
+      <div className="inline-flex relative">
+        <input
+          type="text "
+          className={clsx("p-1 border rounded-xl", inputClassName)}
+        />
+        <button
+          onClick={buttonOnClick}
+          className={clsx(
+            "right-0 absolute p-1 border rounded-xl",
+            buttonClassName
+          )}
+        >
+          搜索
+        </button>
+      </div>
+    </>
+  );
 }
