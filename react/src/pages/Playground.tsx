c68c872c1326709284553a9ee6ae7b61c1eee7e0
kizunaiix <yaodong.sun@outlook.com>
2025-12-23 23:29:27 +0100

    。

--- react/src/pages/Playground.tsx
+++ react/src/pages/Playground.tsx
@@ -4,8 +4,11 @@ import { Input } from "@/components/ui/input";
 function Playground() {
   return (
     <>
-      <main className="flex bg-background h-screen">
-        <div className="flex justify-center items-center gap-2 w-full">
+      <main className="flex flex-col content-center bg-background h-screen">
+        <div className="flex flex-1 justify-center items-center gap-2 bg-amber-300">
+
+        </div>
+        <div className="flex flex-6 justify-center items-center gap-2 bg-amber-400">
           <Input type="text" placeholder="Search" className="w-1/3"/>
           <Button type="submit" variant="default">
             Go
