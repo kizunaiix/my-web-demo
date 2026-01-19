c8fef52969d1a6c187d85c0951a9fba805d2ef5d
kizunaiix <yaodong.sun@outlook.com>
2026-01-19 20:01:46 +0100

    .

--- react/src/components/layout/layout.tsx
+++ react/src/components/layout/layout.tsx
@@ -8,7 +8,7 @@ interface layoutProps {
 }
 
 function Layout({ withSidebar, children }: layoutProps) {
-    
+
   const [isDark, setIsDark] = useState(false);
 
   useEffect(() => {
@@ -19,7 +19,7 @@ function Layout({ withSidebar, children }: layoutProps) {
     <>
       <div className="flex flex-col min-h-dvh">
         <header>header!</header>{" "}
-        {/*  TODO 把isDark传给hearder用于暗黑模式切换按钮*/}
+        {/*  TODO 把isDark传给hearder用于暗黑模式切换按钮 */}
         <div className="flex flex-1">
           {withSidebar && (
             <aside className="hidden md:block bg-sidebar border w-32">
